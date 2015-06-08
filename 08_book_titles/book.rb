665240f5b0f0dd1ae4d763bf5841175c234545ab
Shawn Stovall <stovall.mail@gmail.com>
2015-06-07 22:34:48 -0400

    tweaked solution to 08_book_titles

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -20,34 +20,15 @@ class Book
 end
 
 def conjunction?(word)
-  temp = word.downcase
-
-  if (temp == "and")
-    return true
-  else
-    return false
-  end
+  return word.downcase == "and"
 end
 
 def article?(word)
   temp = word.downcase
-
-  if (word == "the" or
-      word == "a"   or
-      word == "an")
-    return true
-  else
-    return false
-  end
+  return (temp == "the" or temp == "a" or temp == "an")
 end
 
 def preposition?(word)
   temp = word.downcase
-
-  if (word == "in" or
-      word == "of")
-    return true
-  else
-    return false
-  end
+  return (temp == "in" or temp == "of")
 end
